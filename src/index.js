ed5df0520aa3597932d9f39e69ee29ddfcef634d
Beibarys <beibarys0809@mail.ru>
2024-07-31 12:32:18 +0500

    vercel analytics added

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/tailwind.css';
 import AppWrapper from './App';
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.render(
   <React.StrictMode>
     <AppWrapper />
+    <Analytics />
   </React.StrictMode>,
   document.getElementById('root')
 );
